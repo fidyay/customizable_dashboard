c78c3830061898edae69c821cbfc09544cfc4f0b
Fidyay <fidiy1878@gmail.com>
2025-08-07 20:19:51 +0300

    fixed regexp bug

--- src/lib/schemas/hyperLinkParamId.ts
+++ src/lib/schemas/hyperLinkParamId.ts
@@ -5,6 +5,6 @@ const NANOID_SIZE = 21;
 export const hyperLinkParamIdSchema = z
   .string()
   .length(NANOID_SIZE)
-  .regex(/[^\w\-]/);
+  .regex(/^[A-Za-z0-9_-]+$/);
 
 export type THyperLinkParamIdSchema = z.infer<typeof hyperLinkParamIdSchema>;
